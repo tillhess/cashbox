3489645cf60d40312a697070db5f36ee0b7f10b7
Till Hess <tillhess@googlemail.com>
2017-02-13 17:23:26 +0100

    Add ps database

--- app.js
+++ app.js
@@ -10,15 +10,27 @@
 /* jshint node: true, devel: true */
 'use strict';
 
-console.log('main!!');
-
 const
   bodyParser = require('body-parser'),
   config = require('config'),
   crypto = require('crypto'),
   express = require('express'),
   https = require('https'),
-  request = require('request');
+  request = require('request'),
+  pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
